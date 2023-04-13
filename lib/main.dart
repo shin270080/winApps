253561c35862d4342c9c73ff2583ca609452f664
shin270080 <61733235+shin270080@users.noreply.github.com>
2023-04-13 23:11:41 +0900

    シンプル計算機

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,209 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-
-void main() {
-  runApp(MyApp());
-}
-
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Button Press Game',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Button Press Game'),
-        ),
-        body: Center(
-          child: ButtonPressGame(),
-        ),
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: Calculator(),
     );
   }
 }
 
-
-class ButtonPressGame extends StatefulWidget {
+class Calculator extends StatefulWidget {
   @override
-  _ButtonPressGameState createState() => _ButtonPressGameState();
+  _CalculatorState createState() => _CalculatorState();
 }
 
-class _ButtonPressGameState extends State<ButtonPressGame> {
-  int _score = 0;
-  Timer? _timer;
-  int _start = 10;
+class _CalculatorState extends State<Calculator> {
+  String _output = "0";
 
-  void _incrementScore() {
-    setState(() {
-      _score++;
-    });
-    _restartTimer();
-  }
+  String _currentNumber = "0";
+  String _previousNumber = "";
+  String _operation = "";
 
-  void _restartTimer() {
-    if (_timer != null) {
-      _timer?.cancel();
+  void buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _output = "0";
+      _currentNumber = "0";
+      _previousNumber = "";
+      _operation = "";
+    } else if (buttonText == "+/-") {
+      _currentNumber.startsWith("-")
+          ? _currentNumber = _currentNumber.substring(1)
+          : _currentNumber = "-" + _currentNumber;
+    } else if (buttonText == "%" && _currentNumber != "") {
+      double currentNumber = double.parse(_currentNumber);
+      double result = currentNumber / 100;
+      _output = result.toString();
+      _currentNumber = result.toString();
+    } else if (buttonText == "." && _currentNumber.contains(".")) {
+      return;
+    } else if (buttonText == ".") {
+      _currentNumber += buttonText;
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "x" ||
+        buttonText == "/") {
+      double currentNumber = double.parse(_currentNumber);
+
+      if (_previousNumber != "") {
+        switch (_operation) {
+          case "+":
+            _output = (double.parse(_previousNumber) + currentNumber).toString();
+            break;
+          case "-":
+            _output = (double.parse(_previousNumber) - currentNumber).toString();
+            break;
+          case "x":
+            _output = (double.parse(_previousNumber) * currentNumber).toString();
+            break;
+          case "/":
+            _output = (double.parse(_previousNumber) / currentNumber).toString();
+            break;
+        }
+        _previousNumber = _output;
+      } else {
+        _previousNumber = _currentNumber;
+      }
+
+      _operation = buttonText;
+      _currentNumber = "";
+    } else if (buttonText == "=") {
+      if (_previousNumber == "") {
+        return;
+      }
+      double currentNumber = double.parse(_currentNumber);
+      switch (_operation) {
+        case "+":
+          _output = (double.parse(_previousNumber) + currentNumber).toString();
+          break;
+        case "-":
+          _output = (double.parse(_previousNumber) - currentNumber).toString();
+          break;
+        case "x":
+          _output = (double.parse(_previousNumber) * currentNumber).toString();
+          break;
+        case "/":
+          _output = (double.parse(_previousNumber) / currentNumber).toString();
+          break;
+      }
+      _previousNumber = "";
+      _currentNumber = _output;
+    } else {
+      _currentNumber += buttonText;
     }
 
     setState(() {
-      _start = 10;
+      _output = _currentNumber;
     });
+  }
 
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (_start == 0) {
-          timer.cancel();
-        } else {
-          _start--;
-        }
-      });
-    });
+  Widget buildButton(String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+        height: MediaQuery.of(context).size.height * 0.1,
+        width: MediaQuery.of(context).size.width * 0.25,
+    color: buttonColor,
+    child: TextButton(
+    // shape: RoundedRectangleBorder(
+    // borderRadius: BorderRadius.circular(0.0),
+    // side: BorderSide(
+    //  color: Colors.white,
+    //  width: 1,
+//     style: BorderStyle.solid,
+    // ),
+
+    //padding: EdgeInsets.all(16.0),
+    onPressed: () => buttonPressed(buttonText),
+    child: Text(
+    buttonText,
+    style: TextStyle(
+    fontSize: 30.0,
+    fontWeight: FontWeight.normal,
+    color: Colors.white,
+    ),
+    ),
+    ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          'Press the button as many times as you can in 10 seconds!',
-          style: TextStyle(fontSize: 20.0),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 20.0),
-        Text(
-          'Time remaining: $_start seconds',
-          style: TextStyle(fontSize: 40.0),
-        ),
-        SizedBox(height: 20.0),
-        Text(
-          'Score: $_score',
-          style: TextStyle(fontSize: 40.0),
-        ),
-        SizedBox(height: 20.0),
-        ElevatedButton(
-          onPressed: _incrementScore,
-          child: Text(
-            'Press me!',
-            style: TextStyle(fontSize: 30.0,
-          color: Color.fromARGB(255, Random().nextInt(256), Random().nextInt(256), Random().nextInt(256)),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(
+              vertical: 24.0,
+              horizontal: 12.0,
+            ),
+            child: Text(
+              _output,
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-        ),
-      ],
+          Expanded(
+            child: Divider(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              buildButton("7", 1, Colors.grey),
+              buildButton("8", 1, Colors.grey),
+              buildButton("9", 1, Colors.grey),
+              buildButton("/", 1, Colors.orange),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              buildButton("4", 1, Colors.grey),
+              buildButton("5", 1, Colors.grey),
+              buildButton("6", 1, Colors.grey),
+              buildButton("x", 1, Colors.orange),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              buildButton("1", 1, Colors.grey),
+              buildButton("2", 1, Colors.grey),
+              buildButton("3", 1, Colors.grey),
+              buildButton("-", 1, Colors.orange),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              buildButton(".", 1, Colors.grey),
+              buildButton("0", 1, Colors.grey),
+              buildButton("+/-", 1, Colors.grey),
+              buildButton("+", 1, Colors.orange),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              buildButton("C", 1, Colors.red),
+              buildButton("%", 1, Colors.grey),
+              buildButton("", 1, Colors.grey),
+              buildButton("=", 1, Colors.orange),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
